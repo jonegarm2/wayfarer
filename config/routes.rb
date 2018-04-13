75fdf09db9a7e443c2c08df62344db5068d9246f
Jonathan Garman <jonjon@Taylors-MacBook-Pro-2.local>
2018-04-13 15:51:42 -0700

    login/out

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   get "/login", to: "sessions#new"
+  # delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
