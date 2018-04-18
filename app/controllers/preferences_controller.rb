c28984552f558f9379d3dcba2d9ebe9f0a588b54
Jonathan Garman <jonjon@Taylors-MacBook-Pro-2.local>
2018-04-18 10:44:11 -0700

    syntax error

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,4 +1,4 @@
-class PreferencesController > ApplicationController
+class PreferencesController < ApplicationController
     def create
         current_user.destinations << Destination.find(params[:destination_id])
             
